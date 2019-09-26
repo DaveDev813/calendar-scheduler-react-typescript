d9bc30b0d6582d5a8995aa75ef10684a2229a1e3
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-09-26 21:23:09 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import BookCalendar from "./components/BookCalendar";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <BookCalendar />
+    </div>
+  );
+};
+
+export default App;
